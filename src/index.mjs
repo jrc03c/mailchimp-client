4c8611b53b44535b9160afb93f9021f10c8f1cac
jrc03c <jrc03c@pm.me>
2025-07-22 15:56:17 -0500

    ...

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,29 @@
 class MailchimpClient {
+  apiKey = ""
+  serverPrefix = ""
+
   constructor(options) {
     options = options || {}
+    this.apiKey = options.apiKey || this.apiKey
+    this.serverPrefix = options.serverPrefix || this.serverPrefix
+  }
+
+  get baseUrl() {
+    return `https://${this.serverPrefix}.api.mailchimp.com/3.0`
+  }
+
+  async sendRequest(url, options) {
+    options = options || {}
+
+    if (!options.headers) {
+      options.headers = {}
+    }
+
+    if (!options.headers.Authorization) {
+      options.headers.Authorization = `Bearer ${this.apiKey}`
+    }
+
+    return fetch(url, options)
   }
 }
 
