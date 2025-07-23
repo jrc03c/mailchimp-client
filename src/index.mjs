895b4601535c0bce6651a5df4b6d6840f37ae806
jrc03c <jrc03c@pm.me>
2025-07-23 09:52:00 -0500

    ...

--- src/index.mjs
+++ src/index.mjs
@@ -6,10 +6,13 @@ import { findTags } from "./find-tags.mjs"
 import { getAllMembersInSegment } from "./get-all-members-in-segment.mjs"
 import { getAllMembersWithTag } from "./get-all-members-with-tag.mjs"
 import { getMemberInfo } from "./get-member-info.mjs"
+import { getMemberStatus } from "./get-member-status.mjs"
 import { getTagId } from "./get-tag-id.mjs"
 import { pause } from "@jrc03c/pause"
 import { removeMemberFromList } from "./remove-member-from-list.mjs"
 import { removeTagFromMemberProfile } from "./remove-tag-from-member-profile.mjs"
+import { setMemberInfo } from "./set-member-info.mjs"
+import { setMemberStatus } from "./set-member-status.mjs"
 import { urlPathJoin } from "@jrc03c/js-text-tools"
 
 class MailchimpClient {
@@ -35,9 +38,12 @@ class MailchimpClient {
     this.getAllMembersInSegment = getAllMembersInSegment.bind(this)
     this.getAllMembersWithTag = getAllMembersWithTag.bind(this)
     this.getMemberInfo = getMemberInfo.bind(this)
+    this.getMemberStatus = getMemberStatus.bind(this)
     this.getTagId = getTagId.bind(this)
     this.removeMemberFromList = removeMemberFromList.bind(this)
     this.removeTagFromMemberProfile = removeTagFromMemberProfile.bind(this)
+    this.setMemberInfo = setMemberInfo.bind(this)
+    this.setMemberStatus = setMemberStatus.bind(this)
   }
 
   get baseUrl() {
